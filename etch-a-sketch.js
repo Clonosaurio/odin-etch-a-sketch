ecb33fca2746c1b9be0be76d157c0839abdb34de
Clonosaurio <96928935+Clonosaurio@users.noreply.github.com>
2022-03-18 23:27:23 -0300

    added code for 'pixel' generation

---
+++ etch-a-sketch.js
@@ -0,0 +1,18 @@
+let screen = document.querySelector(".screen");
+
+let size = 10;
+
+for(let i = 0; i < size; i++){
+    let column = document.createElement("div");
+    column.className = `column`;
+    column.id = `column${i}`;
+    column.style.flexDirection = "column";
+    screen.appendChild(column);
+    
+    for(let j = 0; j < size; j++){
+        let pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.id = `x${i+1}y${j+1}`;
+        document.querySelector(`#column${i}`).appendChild(pixel);
+    }
+}
